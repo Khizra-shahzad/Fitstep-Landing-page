13d6893c48d5847a313f7269b723c537c8f0682f
Khizra-shahzad <khizrashahzad22@gmail.com>
2025-08-14 18:12:58 +0500

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+const Hero=()=>{
+    return <main className="hero container">
+        <div className="hero-content">
+            <h1>Your Feet Deserve<br></br> The Best</h1>
+            <p>Your feet deserve the best.<br></br>Not only the besttes so purchase a shoes according to your limit.<br></br> Hope so You understand.</p>
+        
+        <div className="hero-btn">
+            <button>Shop now</button>
+            <button className="second-btn">Category</button>
+        </div>
+        <div className="shopping">
+            <p>Available on!</p>
+        </div>
+        <div className="brand-icon">
+        <img src="/images/khizama.png"/>
+        <img src="/images/chrome.jpeg"/> 
+        </div>
+        </div>
+        <div className="hero-img">
+                <img src="/images/imag.jpeg"/>   
+         </div>
+    </main>
+}
+export default Hero
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+const Navigation=()=>{
+    return <div>
+         <nav className="container">
+      <div className="logo">
+        <img src="/images/nikeee.png"/>
+      </div>
+      <ul>
+        <li href="#">menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Account</li>
+      </ul>
+      <button>Login</button>
+    </nav>
+    </div>
+}
+export default Navigation
