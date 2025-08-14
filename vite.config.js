01f13edb5cc4d6e697a3d7857fbbaa44374a37c2
Khizra-shahzad <khizrashahzad22@gmail.com>
2025-08-14 18:20:29 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from "process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: Process.env.VITE_BASE_PATH || '/Fitstep-Landing-page'
+  base: process.env.VITE_BASE_PATH || '/Fitstep-Landing-page',
 })
